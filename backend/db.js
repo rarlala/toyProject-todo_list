b9a338d8ab60430f4e82843df1b2331b1c0a4fd0
민재 KIM <62498196+bapbodanbbang@users.noreply.github.com>
2021-07-28 23:03:11 +0900

    Revert "Add backend structure"

--- backend/db.js
+++
@@ -1,8 +0,0 @@
-const { Sequelize } = require('sequelize');
-
-const sequelize = new Sequelize('Todo', 'root', 'alswo0613!', {
-  host: 'localhost',
-  dialect: 'mysql'
-});
-
-module.exports = { sequelize };
