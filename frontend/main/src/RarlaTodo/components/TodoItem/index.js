1ba891a05a7078d45e94730e6cae7403c9c1a028
rarlaWiz <rarla@wizschool.io>
2021-08-09 04:04:50 +0900

    Fix classname issue

--- frontend/main/src/RarlaTodo/components/TodoItem/index.js
+++ frontend/main/src/RarlaTodo/components/TodoItem/index.js
@@ -6,7 +6,7 @@ export default function TodoItem(props) {
   
   return (
     <li>
-      <span className={todo.complete && "completeTitle"}>{todo.index}. {todo.todoTitle}</span>
+      <span className={todo.complete ? "completeTitle" : ""}>{todo.index}. {todo.todoTitle}</span>
       <div>
         <button onClick={() => onClickComplete(todo.index)}>{todo.complete ? "완료" : "미완료"}</button>
         <button onClick={()=> onClickEdit(todo.index, todo.todoTitle)}>수정</button>
