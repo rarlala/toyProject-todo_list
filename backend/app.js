7e553fa17d750ad323680cdd9e91279b7566e502
homeswiz <homes@wizschool.io>
2021-08-12 19:16:45 +0900

    Add cors

--- backend/app.js
+++ backend/app.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const app = express();
 const db = require("./db");
+const cors = require("cors");
 db.sequelize.sync();
 const port = 3000;
 
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
